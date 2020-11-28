5920d1725710c2a19fb2a3d5badf3f1355ecc6d1
Paras Sood <parassd@gmail.com>
2020-11-29 00:20:21 +1100

    section 2 completed

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-app.get('/',(req,res) => res.send('API Running'));
+//Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 //this is where it can deploy on hiroku. if there is nothing then listen to local host 5000
 const PORT = process.env.PORT || 5000;
 // listen to the port
